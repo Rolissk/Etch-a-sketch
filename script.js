35450f2cbb4cfd30e1b6441f564349a29968f656
Rolissk <162630063+Rolissk@users.noreply.github.com>
2024-06-25 08:39:39 +0300

    Add changable grid size.
    Created script to take human input from 16 to 100 and change the grid
    size. If input is outsite then error is returned.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const playArea = document.getElementById('mainContainer');
 const size = 16 ** 2
+const input = document.getElementById('inputBox').value;
+const btn = document.getElementById('sizeBtn');
+
 function createGrid(size) {
+  clearGrid();
   for (var i = 0; i < size; i++) {
     const box = document.createElement('div');
     box.id = i;
+    const pixelSize = (600 / (Math.pow(size, 0.5)))
+    box.style.height = `${pixelSize}px`;
+    box.style.width = `${pixelSize}px`;
     playArea.appendChild(box);
   }
 };
@@ -12,3 +19,27 @@ createGrid(size)
 playArea.addEventListener("mouseover", (event) => {
   event.target.style.backgroundColor = "red";
 });
+
+btn.addEventListener("click", () => {
+  const input = document.getElementById('inputBox').value;
+  const newSize = input ** 2
+  if (input > 100 || input < 16) {
+    alert("Please enter a number between 16 and 100")
+  } else {
+    console.log(newSize)
+    createGrid(newSize)
+  }
+})
+
+function clearGrid() {
+  const boxes = document.querySelectorAll('div');
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "white";
+  })
+  let e = document.getElementById("mainContainer");
+  let child = e.lastElementChild;
+  while (child) {
+    e.removeChild(child);
+    child = e.lastElementChild;
+  }
+}
